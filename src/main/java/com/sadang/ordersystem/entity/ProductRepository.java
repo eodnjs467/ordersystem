c5f04649bef63adb104322c8fbd22156354462ac
dlwlgh9412 <dlwlgh9412@gmail.com>
2021-07-12 21:01:50 +0900

    Product 검색 인터페이스 구현

--- src/main/java/com/sadang/ordersystem/entity/ProductRepository.java
+++ src/main/java/com/sadang/ordersystem/entity/ProductRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("select p from Product p where p.productName like ':input'")
-    List<Product> findAllByLikeInput(String input);
+    @Query("select p from Product p where p.productName like ':search'")
+    List<Product> findAllBySearch(String search);
 }
