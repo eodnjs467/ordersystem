34286aa35eecae5ec84afb1decf235fe4488bde2
dlwlgh9412 <dlwlgh9412@gmail.com>
2021-07-12 21:00:26 +0900

    RequestMapping 수정 및 검색결과 반환 메서드 작성

--- src/main/java/com/sadang/ordersystem/controller/Controller.java
+++ src/main/java/com/sadang/ordersystem/controller/Controller.java
@@ -1,18 +1,27 @@
 package com.sadang.ordersystem.controller;
 
+import com.sadang.ordersystem.entity.Product;
 import com.sadang.ordersystem.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/product")
 public class Controller {
     private final ProductService productService;
 
-    @GetMapping("search")
-    public ResponseEntity<?> searchResult(String input) {
-        return productService.loadProduct(input);
+    @GetMapping("/search")
+    public ResponseEntity<?> searchResult(@RequestBody String input) {
+        List<Product> productList = productService.loadProduct(input);
+        return productList != null ? new ResponseEntity<>(productList, HttpStatus.OK) : new ResponseEntity<>(
+                "검색결과가 없습니다.", HttpStatus.OK);
     }
 }
